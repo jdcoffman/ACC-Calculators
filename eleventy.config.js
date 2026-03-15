4dbc000e82dab5428f0a9bcc624385ff6dd18f56
Jonathan Coffman <jonathan.coffman@gmail.com>
2026-03-14 22:37:10 -0500

    Add files via upload

---
+++ eleventy.config.js
@@ -0,0 +1,28 @@
+export default function(eleventyConfig) {
+  // Pass through static assets
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy("src/calculators");
+  eleventyConfig.addPassthroughCopy("src/CNAME");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/_headers");
+
+  // Ignore calculator HTML from template processing (they are standalone)
+  eleventyConfig.ignores.add("src/calculators/**");
+
+  // Add a year shortcode for copyright
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      data: "_data"
+    },
+    templateFormats: ["njk", "md"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+};
